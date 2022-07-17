ae91d819aa97a4bda0fd9fb941351306f66740a7
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-07-17 06:58:28 -0400

    simple template completed before resolving CORS issues

---
+++ index.js
@@ -0,0 +1,14 @@
+const userInputForm = document.querySelector(".user-input-form")
+const userQuestionHTMLElement = document.querySelector(".user-question");
+const apiResponseField = document.querySelector(".api-response-field")
+
+const main = async (e) => {
+    e.preventdefault
+    const userQuestionString = userQuestionHTMLElement.value;
+    console.log(userQuestionString);
+    const response = await fetch(`https://us-east4-glassy-signal-354012.cloudfunctions.net/get_answer/?message=${userQuestionString}`, {mode: 'cors'})
+    console.log("received response");
+    apiResponseField.innerHTML(response.json());
+};
+
+userInputForm.addEventListener("click", main);
